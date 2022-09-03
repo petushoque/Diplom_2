c4601cc6c45c0434e823ecce7716d706656e8472
Kubasov Alexei <kubasovalexei@yandex.ru>
2022-09-03 12:14:41 +0700

    Add tests for changing email and password

--- src/test/java/site/nomoreparties/stellarburgers/EditUserTest.java
+++ src/test/java/site/nomoreparties/stellarburgers/EditUserTest.java
@@ -36,7 +36,7 @@ public class EditUserTest {
     @Test
     @DisplayName("Check status code and body of /user: The user is authorized, name change")
     @Description("A test for a positive scenario, a successful server response is 200, the response body contains success: true")
-    public void editUserWithAuthTest(){
+    public void editUserWithAuthChangeNameTest(){
         User editedUser = user;
         String bearerToken = userClient.loginUser(user)
                 .then()
@@ -57,11 +57,97 @@ public class EditUserTest {
     }
 
     @Test
-    @DisplayName("Check status code and body of /user: The user is unauthorized")
+    @DisplayName("Check status code and body of /user: The user is authorized, email change")
+    @Description("A test for a positive scenario, a successful server response is 200, the response body contains success: true")
+    public void editUserWithAuthChangeEmailTest(){
+        User editedUser = user;
+        String bearerToken = userClient.loginUser(user)
+                .then()
+                .extract()
+                .path("accessToken");
+        bearerToken = bearerToken.split(" ")[1];
+        editedUser.setEmail("johnsmith@topsecret.com");
+        boolean isEdited = userClient.editUser(editedUser, bearerToken)
+                .then()
+                .log()
+                .all()
+                .assertThat()
+                .statusCode(SC_OK)
+                .extract()
+                .path("success");
+        Assert.assertTrue(isEdited);
+        System.out.println("The user has been successfully edited");
+    }
+
+    @Test
+    @DisplayName("Check status code and body of /user: The user is authorized, password change")
+    @Description("A test for a positive scenario, a successful server response is 200, the response body contains success: true")
+    public void editUserWithAuthChangePasswordTest(){
+        User editedUser = user;
+        String bearerToken = userClient.loginUser(user)
+                .then()
+                .extract()
+                .path("accessToken");
+        bearerToken = bearerToken.split(" ")[1];
+        editedUser.setPassword("SeCuRiTyLeVeL100");
+        boolean isEdited = userClient.editUser(editedUser, bearerToken)
+                .then()
+                .log()
+                .all()
+                .assertThat()
+                .statusCode(SC_OK)
+                .extract()
+                .path("success");
+        Assert.assertTrue(isEdited);
+        System.out.println("The user has been successfully edited");
+    }
+
+    @Test
+    @DisplayName("Check status code and body of /user: The user is unauthorized, name change")
     @Description("A test for a negative scenario, for a request without auth token, the system responds with a 401 code and an error message")
-    public void editUserWithoutAuthTest(){
+    public void editUserWithoutAuthChangeNameTest(){
         User editedUser = user;
         editedUser.setName("John");
+        boolean isEdited = userClient.editUser(editedUser, "")
+                .then()
+                .log()
+                .all()
+                .assertThat()
+                .statusCode(SC_UNAUTHORIZED)
+                .assertThat()
+                .body("message", equalTo("You should be authorised"))
+                .extract()
+                .path("success");
+        Assert.assertFalse(isEdited);
+        System.out.println("The system did not allow editing the user");
+    }
+
+    @Test
+    @DisplayName("Check status code and body of /user: The user is unauthorized, email change")
+    @Description("A test for a negative scenario, for a request without auth token, the system responds with a 401 code and an error message")
+    public void editUserWithoutAuthChangeEmailTest(){
+        User editedUser = user;
+        editedUser.setEmail("johnsmith@topsecret.com");
+        boolean isEdited = userClient.editUser(editedUser, "")
+                .then()
+                .log()
+                .all()
+                .assertThat()
+                .statusCode(SC_UNAUTHORIZED)
+                .assertThat()
+                .body("message", equalTo("You should be authorised"))
+                .extract()
+                .path("success");
+        Assert.assertFalse(isEdited);
+        System.out.println("The system did not allow editing the user");
+    }
+
+    @Test
+    @DisplayName("Check status code and body of /user: The user is unauthorized, password change")
+    @Description("A test for a negative scenario, for a request without auth token, the system responds with a 401 code and an error message")
+    public void editUserWithoutAuthChangePasswordTest(){
+        User editedUser = user;
+        editedUser.setPassword("SeCuRiTyLeVeL100");
         boolean isEdited = userClient.editUser(editedUser, "")
                 .then()
                 .log()
